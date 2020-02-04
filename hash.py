9f1aef37297e5e42887b2e829985eaca2c283ee0
Tim Hannifan <hannifan@gmail.com>
2020-02-04 15:46:46 -0600

    adding a return value to hasher

--- hash.py
+++ hash.py
@@ -14,6 +14,20 @@ from functools import partial
 
 BLOCK_SIZE = 1024
 
+def import_hashlib():
+    # Check that hashlib can be imported
+    try:
+        import hashlib
+    except ModuleNotFoundError:
+        sys.exit('Error: hashlib not found.')
+
+def generate_sha256(fpath):
+    # import_hashlib()
+
+    if not os.path.isfile(fpath):
+        sys.exit('Error: Supplied filepath does not refer a valid file.')
+    return run(fpath)
+
 def run(fpath):
     """Prints the sha256 hexdigest of a file.
 
@@ -21,34 +35,33 @@ def run(fpath):
       fpath: A filepath for the file to be hashed.
 
     Returns:
-      Nothing
+      TODO
 
     Raises:
       ModuleNotFoundError: If hashlib is not found
     """
-
-    hasher = hashlib.sha256()
-
-    with open(fpath, 'rb') as file:
-        for chunk in iter(partial(file.read, BLOCK_SIZE), b''):
-            hasher.update(chunk)
-
-    print('{}  {}'.format(hasher.hexdigest(), os.path.basename(fpath)))
-
-
-if __name__ == '__main__':
-    # Check passed arguments
-    if len(sys.argv) < 2:
-        sys.exit('Error: No filepath supplied in arguments.')
-    elif len(sys.argv) > 2:
-        sys.exit('Error: Too many arguments supplied.')
-    if not os.path.isfile(sys.argv[1]):
-        sys.exit('Error: Supplied filepath does not refer a valid file.')
-
-    # Check that hashlib can be imported
+    # Check that hashlib is available
     try:
         import hashlib
     except ModuleNotFoundError:
         sys.exit('Error: hashlib not found.')
+
+    # Initialize hasher and iterate through blocks of file
+    hasher = hashlib.sha256()
+    with open(fpath, 'rb') as file:
+        for chunk in iter(partial(file.read, BLOCK_SIZE), b''):
+            hasher.update(chunk)
+
+    # print('{}  {}'.format(hasher.hexdigest(), os.path.basename(fpath)))
+    return (hasher.hexdigest(), os.path.basename(fpath))
+
+if __name__ == '__main__':
+    # Check CLI arguments, correct command is "python3 hash.py <filepath>"
+    if len(sys.argv) < 2:
+        sys.exit('Error: No filepath supplied in arguments.')
+    elif len(sys.argv) > 2:
+        sys.exit('Error: Too many arguments supplied.')
+    elif not os.path.isfile(sys.argv[1]):
+        sys.exit('Error: Supplied filepath does not refer a valid file.')
 
     run(sys.argv[1])
