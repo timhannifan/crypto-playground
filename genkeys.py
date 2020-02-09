40428941df20e765cc0f0b919bbe47bf6718eb99
Tim Hannifan <hannifan@gmail.com>
2020-02-09 00:38:13 -0600

    cleanup on aisle keygeneration

--- genkeys.py
+++ genkeys.py
@@ -10,14 +10,15 @@ BLOCK_SIZE = 48
 NONCE_SIZE = 128 // 8
 DEMO_U_NAME = 'timhannifan'
 DEMO_PWD = 'password123'
-KEY_TYPE = 'encr_key'
+TEST_FILE = 'testdata/small.txt'
+CREDENTIALS_STORE = 'credentials.txt'
 
 def generate_key(key_type, u_name, password):
     salt_b = bytes(key_type, 'utf-8') + os.urandom(SALT_SIZE)
     password_b = bytes(password, 'utf-8')
     dk = hashlib.pbkdf2_hmac('sha256', password_b, salt_b, N_ITERATIONS)
 
-    return (u_name, dk, salt_b)
+    return (u_name, key_type, dk, salt_b.hex())
 
 
 def get_keyhash(val):
@@ -27,35 +28,18 @@ def get_keyhash(val):
     return hasher.hexdigest()
 
 
-def write_credentials(u_name, salt, key_hash, key_type, fname):
-    with open(fname, 'wa') as file:
-        line = ':'.join([u_name, key_type, salt, key_hash])
-        file.write(line)
+def write_credentials(credentials, fname):
+
+    with open(fname, 'w') as file:
+        for key_creds in credentials:
+            print('keycred')
+            user, key_type, dk, salt_hex = key_creds
+            key_hash = get_keyhash(dk)
+            line = ':'.join([user, key_type, key_hash, salt_hex]) + '\n'
+            file.write(line)
 
 
-key_type = 'msg'
-creds = generate_key(key_type, DEMO_U_NAME, DEMO_PWD)
-u_name, dk, salt = creds
-write_credentials(u_name,  key_type, salt.hex(), get_keyhash(dk), 'creds.txt')
+msg_creds = generate_key('msg', DEMO_U_NAME, DEMO_PWD)
+sig_creds = generate_key('sig', DEMO_U_NAME, DEMO_PWD)
 
-key_type = 'sig'
-creds = generate_key(key_type, DEMO_U_NAME, DEMO_PWD)
-u_name, dk, salt = creds
-write_credentials(u_name,  key_type, salt.hex(), get_keyhash(dk), 'creds.txt')
-
-
-
-
-# counter = 1
-# with open('testdata/small.txt', 'rb') as file:
-#     nonce = os.urandom(NONCE_SIZE)
-#     for chunk in iter(partial(file.read, BLOCK_SIZE), b''):
-#         print('ROUND')
-#         # print((len(encr_key)))
-#         print(len(nonce) + len(encr_key) + len(bytes(counter)))
-#         print(bytes(counter))
-#         print(len(bytes(chunk)))
-
-#         # size_diff = (len(nonce) + len(encr_key)) - len(bytes(chunk))
-#         # ks = generate_keystream(encr_key, nonce, counter)
-#         counter += 1
+write_credentials([msg_creds, sig_creds], CREDENTIALS_STORE)
