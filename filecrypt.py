57d7a0a060c7cdc5a80fc1540d2c4b0b283e8566
Tim Hannifan <hannifan@gmail.com>
2020-02-09 23:00:31 -0600

    completing roundtrip with user-prompted key input

--- filecrypt.py
+++ filecrypt.py
@@ -11,6 +11,7 @@ Author: Tim Hannifan
 import sys
 import os
 import re
+import json
 from hash import generate_sha256_hash
 from genkeys import generate_credentials, get_private_key, get_keyhash, get_private_keys
 from encrypt import write_ciphertext
@@ -20,7 +21,7 @@ CREDENTIALS_STORE = 'credentials.txt'
 USER = 'timhannifan'
 PWD = 'password123'
 BLOCK_SIZE = 32
-
+HASH_RECORD = 'hashes.json'
 
 def get_user_credentials(username, password, credentials_store):
     # generate keys if they don't exist
@@ -61,31 +62,24 @@ def write_output(data, fpath):
     with open(fpath, 'a') as file:
         file.write(data)
 
-def sign_output(fpath, sig_key):
-    print('Signing output...')
-    signed_ciphertext_hash = generate_sha256_hash(fpath, sig_key)
+def verify_integrity(fname, sig_key):
+    with open(HASH_RECORD) as json_file:
+        data = json.load(json_file)
+        
+        if ((fname in data) and 
+            (generate_sha256_hash(fname, sig_key ) == data[fname])):
+            print('Integrity check successful')
+            return True
+        return False
 
-    write_output(signed_ciphertext_hash, fpath)
+def write_authentication_tag(output_file, sig_key):
+    tag = generate_sha256_hash(output_file, sig_key)
+    record = {}
+    record[output_file] = tag
 
-def verify_ciphertext(fpath, sig_key):
-    #TODO
-    # signed_ciphertext_hash = generate_sha256_hash(fpath, sig_key)
-    return True
+    with open(HASH_RECORD, 'w') as file:
+        json.dump(record, file)
 
-def decrypt(input_file, msg_key, output_file):
-    if os.path.exists(output_file):
-        os.remove(output_file)
-    with open(input_file, 'rb') as file:
-        data = file.read()
-        plaintext = xor(chunk, msg_key)
-        print(plaintext.decode("utf8"))
-        # for chunk in iter(partial(file.read, BLOCK_SIZE), b''):
-        #     plaintext = xor(chunk, msg_key)
-
-        #     print(plaintext.decode("utf8"))
-            # print(type(plaintext))
-            # print(plaintext.decode("utf8"))
-            # write_output(ciphertext, output_file)
 
 def run(command, input_file, output_file):
     """TODO
@@ -103,10 +97,13 @@ def run(command, input_file, output_file):
     """
     if command == 'encrypt':
         print('Encrypting file...')
+        if os.path.exists(output_file):
+            os.remove(output_file)
         # credentials = get_user_credentials(USER, PWD, CREDENTIALS_STORE)
         msg_keys = get_private_keys(PWD)
         sig_keys = get_private_keys(PWD)
         print('msgkey:', msg_keys[1])
+        print('sigkey:', sig_keys[1])
 
         with open(input_file, 'r') as file:
             message = file.read()
@@ -116,82 +113,22 @@ def run(command, input_file, output_file):
         cipher_text = xor(message, key)
 
         write_output(cipher_text.hex(), output_file)
-        # message = 'This is a secret message'
-        # key = msg_keys[0]
-        # cipherText = xor_strings(message.encode('utf8'), key)
-        # print('cipherText:', cipherText)
-        # print('decrypted:', xor_strings(cipherText, key).decode('utf8'))
-        # if os.path.exists(output_file):
-        #     os.remove(output_file)
-        # with open(input_file, 'rb') as file:
-        #     for chunk in iter(partial(file.read, BLOCK_SIZE), b''):
-        #         ciphertext = xor(chunk, msg_keys[0])
-        #         write_output(ciphertext, output_file)
-
-        # sign_output(output_file, sig_keys[0])
+        write_authentication_tag(output_file, sig_keys[1].encode('utf-8'))
 
     elif command == 'decrypt':
         print('Decrypting file...')
+        user_sig_key = input('Enter secret signature key: ')
+        
+        if not verify_integrity(input_file, user_sig_key.encode('utf-8')):
+            sys.exit('Decryption verification failed.')
 
         with open(input_file, 'r') as file:
             cipher_hex = file.read()
             cipher_text = bytes.fromhex(cipher_hex)
-
             key = bytes.fromhex(input('Secret message key: '))
-
             decrypted = xor(cipher_text, key).decode('utf-8')
             
             write_output(decrypted, output_file)
-
-            # print(decrypted.decode('utf8'))
-            # print(decrypted.decode('utf-8', 'backslashreplace'))
-# 
-        # print('cipher_text:', cipher_text)
-        # print('decrypted:', xor(cipher_text, key).decode('utf8'))
-
-
-        # with open(input_file, 'r') as file:
-        #     ciphertext = file.read()
-        #     ciphertext = bytes.fromhex(ciphertext)
-        #     k = '8bd35428e035b8435d8022755112b3d589dc26b68c802d67e12e72d9c8617ddd'
-        #     k_b = bytes.fromhex(k)
-        #     print(type(ciphertext))
-        #     plain = xor(ciphertext, k_b)
-        #     # print
-        #     print(plain.decode('utf-8'))
-
-
-
-
-        # with open(input_file, 'rb') as file:
-        #     ciphertext = file.read()
-        #     k = '8bd35428e035b8435d8022755112b3d589dc26b68c802d67e12e72d9c8617ddd'
-        #     k_b = bytes.fromhex(k)
-        #     plain = xor(ciphertext, k_b)
-        #     print(plain.decode('utf-8'))
-        # user_sig_key = input('Enter signature key: ')
-
-        # if verify_ciphertext(input_file, user_sig_key):
-        #     user_msg_key = input('Enter message key: ')
-        #     key_bytes = bytes.fromhex(user_msg_key)
-        #     # print(type(key_bytes))
-        #     decrypt(input_file, key_bytes, output_file)
-    elif command == 'test':
-        print('TEST Encrypting file...')
-        # credentials = get_user_credentials(USER, PWD, CREDENTIALS_STORE)
-        msg_keys = get_private_keys(PWD)
-        sig_keys = get_private_keys(PWD)
-
-        # message = 'This is a secret message'
-        with open(input_file, 'r') as file:
-            message = file.read()
-        key = msg_keys[0]
-        cipher_text = xor(message.encode('utf8'), key)
-        print('cipher_text:', cipher_text)
-        print('decrypted:', xor(cipher_text, key).decode('utf8'))
-
-
-
 
 if __name__ == '__main__':
     # Check passed arguments
